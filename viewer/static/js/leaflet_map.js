2028b7b8f286ad9e703312518a9d900cd9117eec
Nir Galon <nirgn975@gmail.com>
2015-11-11 23:03:46 +0200

    add a map layer (hard coded)

--- viewer/static/js/leaflet_map.js
+++ viewer/static/js/leaflet_map.js
@@ -22,4 +22,12 @@ function showPosition(position) {
       id: 'nirgn975.cigmtjxyw000rc3knz02njkhn',
       accessToken: 'pk.eyJ1IjoibmlyZ245NzUiLCJhIjoiY2lnbXRqeTcxMDAwdmx6a3RueGViemV0eCJ9.3wBhw04dxzXHfd56yUfufQ'
   }).addTo(map);
+
+
+  layer = L.tileLayer('http://46.101.211.183:3000/maps/tile/1/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18,
+      id: 'nirgn975.cigmtjxyw000rc3knz02njkhn',
+      accessToken: 'pk.eyJ1IjoibmlyZ245NzUiLCJhIjoiY2lnbXRqeTcxMDAwdmx6a3RueGViemV0eCJ9.3wBhw04dxzXHfd56yUfufQ'
+  }).addTo(map);
 }
