527195ce4cf86f0e604e0bb8b57046c03c679139
Nir Galon <nirgn975@gmail.com>
2015-11-15 23:19:36 +0200

    connect projects (vagrant and postgis db)

--- script.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get install -y python-pip python-dev build-essential postgis postgresql-9.3-postgis-2.1
-cd /srv/website
-sudo pip install -r requirements.txt
-sudo -iu postgres createuser proj123 -S -D -R
-sudo -iu postgres createdb proj123 -O proj123
-sudo -iu postgres psql -c \"alter user proj123 with password 'proj123';\"
-sudo -iu postgres psql proj123 -c \"CREATE EXTENSION postgis;\
-sudo -iu postgres psql proj123 -c \"CREATE EXTENSION postgis_topology;\""
