98738b0872f5f5a6802b7ba28fa0095cd1487107
sole <sbox@gmail.com>
2015-11-24 17:00:22 +0200

    added getAllMaps method agian

--- viewer-app/viewer/views.py
+++ viewer-app/viewer/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from djgeojson.views import GeoJSONLayerView
-
-class MapLayer(GeoJSONLayerView):
-    bbox_auto = True
-
+from viewer.models import MyMaps
+from django.http import JsonResponse
 
 links = {'Home':'/home', 'Contact':'/contact',
          'About':'/about', 'Help':'/help',}
+
 def index(request):
     '''
     This is the index views.
@@ -26,3 +24,12 @@ def privacyAndTerms(request):
     :return:
     '''
     return render(request, 'privacyAndTerms.html', {'links': links})
+
+
+def getAllMaps(request):
+    my_maps = MyMaps.objects.all()
+    url = "http://localhost:3000/maps/tile/{id}/{xyz}.png"
+    return JsonResponse([{
+        'id': map.id,
+        'url': url.format(id=map.id, xyz='{z}/{x}/{y}')
+    } for map in my_maps], safe=False)
